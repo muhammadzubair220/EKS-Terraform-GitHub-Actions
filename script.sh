6f4d757f623ea75bb3b6009f3f4955524382943e
Ubuntu <ubuntu@ip-172-31-26-218.ec2.internal>
2025-02-16 16:19:41 +0000

    Add installation and configuration scripts for application

---
+++ script.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# For Ubuntu 22.04
+
+# Updating package lists
+sudo apt update -y
+
+# Installing Java
+sudo apt install -y openjdk-17-jre openjdk-17-jdk
+java -version
+
+# Installing Jenkins
+sudo apt install -y curl gnupg
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update -y
+sudo apt install -y jenkins
+
+# Installing Docker
+sudo apt install -y docker.io
+sudo usermod -aG docker jenkins
+sudo usermod -aG docker ubuntu
+sudo systemctl restart docker
+sudo chmod 777 /var/run/docker.sock
+
+# Installing Terraform
+sudo apt update -y
+sudo apt install -y gnupg software-properties-common wget
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update -y
+sudo apt install -y terraform
+
+# Installing AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install -y unzip
+unzip awscliv2.zip
+sudo ./aws/install
+
+# Running SonarQube Container
+docker run -d --name sonar -p 9000:9000 sonarqube:community
+
+# Installing Trivy
+sudo apt install -y wget apt-transport-https gnupg lsb-release
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo gpg --dearmor -o /usr/share/keyrings/trivy-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/trivy-keyring.gpg] https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/trivy.list
+sudo apt update -y
+sudo apt install -y trivy
+
